1404f3aa54df1ff1576ce58ed178d3e9ccadc265
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-05-01 12:20:54 -0700

    Improve, refactor, modularize.

--- js/script.js
+++ js/script.js
@@ -1,72 +1,104 @@
-document.getElementById('pdf-form').addEventListener('submit', async (e) => {
+// Event listeners
+document.getElementById('pdf-form').addEventListener('submit', onFormSubmit);
+
+// Event handler functions
+async function onFormSubmit(e) {
   e.preventDefault();
 
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
+  const name = getValueById('name');
+  const email = getValueById('email');
 
   try {
-      const backgroundImage = await fetchImageAsBase64('img/background.png');
-      generatePDF(name, email, backgroundImage);
+    const backgroundImage = await fetchImageAsBase64('img/background.png');
+    generatePDF(name, email, backgroundImage);
   } catch (error) {
-      console.error('Error:', error);
+    console.error('Error:', error);
   }
-});
+}
+
+// Utility functions
+function getValueById(id) {
+  return document.getElementById(id).value;
+}
 
 async function fetchImageAsBase64(url) {
   const response = await fetch(url);
   const blob = await response.blob();
   return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
   });
 }
 
+// PDF generation functions
 async function generatePDF(name, email, backgroundImage) {
   const { jsPDF } = window.jspdf;
   const dpi = 72;
 
-  // Load the background image
-  const img = new Image();
-  img.src = backgroundImage;
-  await new Promise((resolve) => (img.onload = resolve));
+  const img = await loadImage(backgroundImage);
 
-  // Calculate the dimensions in inches
-  const widthInInches = img.width / dpi;
-  const heightInInches = img.height / dpi;
+  const dimensions = calculateDimensions(img, dpi);
 
-  // Create the PDF with the correct dimensions
-  const doc = new jsPDF({
-      orientation: 'landscape',
-      unit: 'in',
-      format: [widthInInches, heightInInches],
+  const doc = createPDF(dimensions);
+
+  addBackgroundImage(doc, backgroundImage, dimensions);
+
+  const fontSize = 16 * 4.16666667;
+  addText(doc, name, email, dimensions, fontSize);
+
+  savePDF(doc);
+}
+
+async function loadImage(src) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve(img);
   });
+}
 
-  // Add the background image
-  doc.addImage(backgroundImage, 'PNG', 0, 0, widthInInches, heightInInches);
+function calculateDimensions(img, dpi) {
+  return {
+    width: img.width / dpi,
+    height: img.height / dpi,
+  };
+}
 
-  // Add the text
-  const fontSize = 16;
+function createPDF(dimensions) {
+  const jsPDF = window.jspdf.jsPDF;
+  return new jsPDF({
+    orientation: 'landscape',
+    unit: 'in',
+    format: [dimensions.width, dimensions.height],
+  });
+}
+
+function addBackgroundImage(doc, backgroundImage, dimensions) {
+  doc.addImage(backgroundImage, 'PNG', 0, 0, dimensions.width, dimensions.height);
+}
+
+function addText(doc, name, email, dimensions, fontSize) {
   doc.setFontSize(fontSize);
 
   const nameText = `Name: ${name}`;
   const emailText = `Email: ${email}`;
 
-  const nameTextWidth = doc.getTextWidth(nameText);
-  const emailTextWidth = doc.getTextWidth(emailText);
+  const nameX = calculateXPosition(doc, nameText, dimensions.width);
+  const emailX = calculateXPosition(doc, emailText, dimensions.width);
 
-  const nameX = (widthInInches - nameTextWidth) / 2;
-  const emailX = (widthInInches - emailTextWidth) / 2;
-
-  const nameY = 725 / dpi;
+  const nameY = 725 / 72;
   const emailY = nameY + fontSize / 72;
 
-  doc.text(nameText, nameX, nameY); // Convert pt to inches
-  doc.text(emailText, emailX, emailY); // Convert pt to inches
-
-  // Save the PDF
-  doc.save('generated.pdf');
+  doc.text(nameText, nameX, nameY);
+  doc.text(emailText, emailX, emailY);
 }
 
+function calculateXPosition(doc, text, width) {
+  return (width - doc.getTextWidth(text)) / 2;
+}
 
+function savePDF(doc) {
+  doc.save('generated.pdf');
+}
